d860bf96ca858f4b583e823c0a1bd9202160ec7f
pimentapedro <pedro@meetmaps.com>
2021-05-22 23:57:56 +0200

    Give state to App + fetch data

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,66 @@
-import Legend from '../Legend/Legend';
+import React from 'react'
+import Legend from '../Legend/Legend'
 
-function App() {
-  return (
+class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      dataUrl: 'https://raw.githubusercontent.com/Vizzuality/front-end-code-challenge/master/data.json',
+      legendItems: [],
+    }
+
+    this.onChangeVisibility = this.onChangeVisibility.bind(this)
+    this.onChangeCollapse = this.onChangeCollapse.bind(this)
+  }
+
+  setLegendItems(items) {
+    // TODO: also build timeline?
+    // filter timeline
+    const filteredItems = items.filter((item) => item.type !== 'timeline')
+
+    const itemsForState = filteredItems.map((item) => {
+      item.visibility = true
+      item.collapse = false
+
+      return (
+        item
+      )
+    })
+
+    itemsForState[3] = {name:'iuygi uygiuy giuyg iuygiu ygiuyg iuygiu ygiuy giuyg iuyg ygiuyg iuygiuy giuyg'}
+
+    this.setState({
+      legendItems: itemsForState,
+    })
+  }
+
+  componentDidMount() {
+    fetch(this.state.dataUrl)
+      .then((response) => {
+        if (response.ok) {
+          return response.json()
+        } else {
+          // TODO: handle error
+          console.log(`Oops! There as an error ${response.status}: ${response.statusText}`)
+        }
+      })
+      .then((data) => {
+        this.setLegendItems(data)
+      })
+      .catch((error) => {
+        console.log('wat', error)
+      })
+  }
+
+  render() {
+    return (
     <div className="App">
       App
       <Legend />
     </div>
-  );
+    )
+  }
 }
 
-export default App;
+export default App
