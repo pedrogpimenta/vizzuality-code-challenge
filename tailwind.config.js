46cee4d5f2fb88c2c03e4b02c3b4b9afbbde5ff7
pimentapedro <pedro@meetmaps.com>
2021-05-23 17:10:26 +0200

    Add Open Sans from Google Fonts + Custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          550: '#565656',
+          650: '#41484E',
+        }
+      },
+    },
+    fontFamily: {
+      sans: '"Open Sans", ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";',
+    },
   },
   variants: {
     extend: {},
